596bca0dc64090921e73395766829db6c0e4de3e
g-praveen29 <guduripraveenkumar@gmail.com>
2025-04-05 23:29:43 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,33 +1,15 @@
-//package com.example.src;
-//import statement declaration
-import java.util.Scanner;
-//Class declaration
-public class Hello{
-// Field/Variable/Attribute/Properties declaration
-private String message;
-//Parameterized Constructor declaration
-public Hello(String message) {
-this.message = message;
-}
-//Main method declaration
-public static void main(String[] args){
-// Object declaration
-Hello program = new Hello("Hello, World!");
-//calling (invoking) of instance method
-program.printMessage();
-//Scanner class object for user input
-Scanner scanner = new Scanner(System.in);
-System.out.print("Enter your name: ");
-//read the user entered input
-String name = scanner.nextLine();
-System.out.println("Hello, " + name + "!");
-//close the scanner object
-scanner.close();
-}
-//method declaration to print message
-public void printMessage()
-{
-//method implementation
-System.out.println(message);
-}
+public class Hello {
+    public static void main(String[] args) {
+        String name = null;
+
+        if (args.length > 0) {
+            name = args[0];
+        } else {
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Enter your name: ");
+            name = sc.nextLine();
+        }
+
+        System.out.println("Hello, " + name);
+    }
 }
